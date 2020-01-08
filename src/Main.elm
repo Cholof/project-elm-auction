4ac44c245884a30b5f395e24f81ade6082decf5f
Chi Trinh <chikun_88@hotmail.com>
2020-01-08 14:31:12 +0100

    Homepage Done

--- src/Main.elm
+++ src/Main.elm
@@ -152,7 +152,7 @@ menu model =
     Navbar.config NavMsg
         |> Navbar.withAnimation 
         |> Navbar.collapseMedium  
-        |> Navbar.info
+        |> Navbar.light
         |> Navbar.brand [ href "#" ]
             [ img
                 [ src "src/assets/images/museum.svg"
@@ -257,12 +257,12 @@ pageHome modelHome =
         ,
         Grid.row [] 
         [ Grid.col []
-            [ Card.config [ Card.outlineDanger ]
+            [ Card.config [ Card.outlineLight ]
                 
                 |> Card.headerH4 [class "text-center" ] [ text "Home" ]
                 |> Card.block []
-                    [ Block.text []
-                     [ text "Todo: Image responsive" ]
+                    [ Block.text [class "text-center"]
+                     [ text "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum" ]
                     ]
                 |> Card.view
             ]
